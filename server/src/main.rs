67da775c9b6c0b4888b52b954b86cdde9f2c1a77
Guilherme Pereira <guilospanck@protonmail.com>
2023-01-06 11:35:35 +0000

    Add more info and examples about the protocol

--- server/src/main.rs
+++ server/src/main.rs
@@ -43,7 +43,23 @@ type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
 use uuid::Uuid;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-pub type Tag = [String; 3]; // ["p", <32-bytes hex of the key>], <recommended relay URL>]  ["e", <32-bytes hex of the id of another event>, <recommended relay URL>]  ...
+pub enum EventTags {
+  PubKey,
+  Event,
+}
+
+impl EventTags {
+  fn as_str(&self) -> &'static str {
+    match self {
+      EventTags::PubKey => "p", // points to a pubkey of someone that is referred to in the event
+      EventTags::Event => "e", // points to the id of an event this event is quoting, replying to or referring to somehow.
+    }
+  }
+}
+
+// ["p", <32-bytes hex of the key>], <recommended relay URL>]  ["e", <32-bytes hex of the id of another event>, <recommended relay URL>]  ...
+// the <recommended relay URL> is optional and can be set to ""
+pub type Tag = [String; 3];
 
 pub enum EventKinds {
   Metadata = 0,
@@ -61,6 +77,23 @@ pub enum EventKinds {
   ChannelMuteUser = 44,
 }
 
+/**
+ * Example (id's and other hashes are not valid for the information presented):
+    ```json
+    {
+      "id": "ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb"
+      "pubkey": "02c7e1b1e9c175ab2d100baf1d5a66e73ecc044e9f8093d0c965741f26aa3abf76",
+      "created_at": 1673002822,
+      "kind": 1,
+      "tags": [
+        ["e", "688787d8ff144c502c7f5cffaafe2cc588d86079f9de88304c26b0cb99ce91c6", "wss://relay.damus.io"],
+        ["p", "02c7e1b1e9c175ab2d100baf1d5a66e73ecc044e9f8093d0c965741f26aa3abf76", ""],
+      ],
+      "content": "Lorem ipsum dolor sit amet",
+      "sig": "e8551d85f530113366e8da481354c2756605e3f58149cedc1fb9385d35251712b954af8ef891cb0467d50ddc6685063d4190c97e9e131f903e6e4176dc13ce7c"
+    }
+    ```
+ */
 pub struct Event {
   id: String, // 32-bytes SHA256 of the serialized event data
   pubkey: String, // 32-bytes hex-encoded public key of the event creator
