e39a1c7c9567f904644263721d1926a4eefa62aa
Guilherme Pereira <guilospanck@protonmail.com>
2023-05-20 11:09:50 +0100

    Change some nginx.conf and debug lines

--- relay/src/send_to_client/mod.rs
+++ relay/src/send_to_client/mod.rs
@@ -10,10 +10,7 @@ pub struct OutboundInfo {
 }
 
 pub fn send_message_to_client(tx: Tx, content: String) {
-  debug!("===============================================================");
-  debug!("Sending message to client:");
   debug!("{content}");
-  debug!("===============================================================");
   tx.unbounded_send(Message::Text(content))
     .unwrap();
 }
