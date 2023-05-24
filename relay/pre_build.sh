5b20037df86ce1d64a61d57d603dc48793713e2d
Guilherme Pereira <guilospanck@protonmail.com>
2023-05-24 10:17:07 +0100

    Fix Deploy workflow
    Change deploy workflow to use cargo workspace

--- relay/pre_build.sh
+++ relay/pre_build.sh
@@ -1,4 +1,10 @@
+# apt-get update
+# apt-get install -y wget build-essential checkinstall zlib1g-dev openssl pkg-config libssl-dev
+# wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.18_amd64.deb
+# dpkg -i libssl1.1_1.1.1f-1ubuntu2.18_amd64.deb
+
 apt-get update
-apt-get install -y wget build-essential checkinstall zlib1g-dev openssl pkg-config libssl-dev
+apt upgrade
+apt-get install -y wget pkg-config libssl-dev musl-tools
 wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.18_amd64.deb
 dpkg -i libssl1.1_1.1.1f-1ubuntu2.18_amd64.deb
