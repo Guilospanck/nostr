f73c4ad61f017b42444a78d7f797e85ee8619e09
Guilherme Pereira <guilospanck@protonmail.com>
2023-05-31 14:21:17 +0100

    Remove init_logger once function from sdk

--- nostr-sdk/src/lib.rs
+++ nostr-sdk/src/lib.rs
@@ -1,17 +1,6 @@
 pub use env_logger::Env;
 pub use log::{debug, info};
 
-use std::sync::Once;
-
-static INIT_LOGGER: Once = Once::new();
-
-fn init_logger() {
-  INIT_LOGGER.call_once(|| {
-    dotenv::dotenv().ok();
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-  });
-}
-
 pub mod client_to_relay_communication;
 pub mod event;
 pub mod filter;
--- nostr-sdk/src/schnorr.rs
+++ nostr-sdk/src/schnorr.rs
@@ -239,8 +239,6 @@ pub fn verify_schnorr<C: Verification>(
 /// can be used for both Schnorr and ECDSA signatures.
 ///
 pub fn generate_keys() -> AsymmetricKeys {
-  super::init_logger();
-
   let secp = Secp256k1::new();
   let mut rng = rand::thread_rng();
 
