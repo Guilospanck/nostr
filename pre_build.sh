e3980151a740dce226c6ae172af5e71c1230cdcf
Guilherme Pereira <guilospanck@protonmail.com>
2023-06-12 18:21:02 +0100

    Update GitHub Workflows

--- pre_build.sh
+++ pre_build.sh
@@ -1,4 +1,4 @@
 apt-get update
 apt-get install -y wget pkg-config libssl-dev musl-tools
-wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.18_amd64.deb
-dpkg -i libssl1.1_1.1.1f-1ubuntu2.18_amd64.deb
+wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
+dpkg -i libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb
